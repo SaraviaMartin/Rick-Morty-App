365b2abed3a4df34ff43b3ad559df0ffc8a0f0c3
Juan Martin Saravia Da Silva <saraviamartin16@hotmail.com>
2023-03-14 15:34:25 -0300

    Repasando lifecycle

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -25,5 +25,3 @@ return (
          })}
          </Cartas>)
 }
-
-
---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,20 @@
+// import React from 'react'
+
+
+// export default function login (){
+
+
+
+
+//     return
+//     <div>
+//     <form>
+//         <label >username</label>
+//         <input name= "username"></input>
+//         <label>password</label>
+//         <input name= "password"></input>
+//         <button>LOGIN</button>
+//     </form>
+//     </div>
+
+// }
